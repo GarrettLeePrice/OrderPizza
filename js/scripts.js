0e1fbeed3ff74637eb91c76f6c694e9444483fd0
Garrett Price <garrettleeprice@gmail.com>
2016-08-26 15:55:11 -0700

    add modal window with embedded video.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,59 @@
-
-function Pizza(size,toppings) {
-  this.size = size;
-  this.toppings = toppings;
-  };
-
-  Pizza.prototype.price = function() {
+function Pizza(size, toppings) {
+    this.size = size;
+    this.toppings = toppings;
+};
+Pizza.prototype.price = function() {
     var price = 15;
-      if (this.toppings === "fishSticks") {
-      price = price + 5
-    }  if (this.toppings === "iceCream") {
-      price = price + 5
-    }  if (this.toppings === "spaghetti") {
-      price = price + 5
-    } if (this.size === "large") {
-      price = price + 10
-    } else if (this.size === "medium") {
-      price = price + 5
-    } else {
-      price === price;
+    if (this.toppings === "fishSticks") {
+        price = price + 5
     }
-      return price;
-  };
-
-  $(document).ready(function() {
-    $("#newPizza").submit(function(event) {
-      $("ul").empty();
-      event.preventDefault();
-      var size = $("input:radio[name=pizzaSize]:checked").val();
-      var toppings = $("input:radio[name=toppings]:checked").val();
-      var myPizza = new Pizza(size,toppings);
-      $("#showPrice").text(" TOTAL $" + myPizza.price());
+    if (this.toppings === "iceCream") {
+        price = price + 5
+    }
+    if (this.toppings === "spaghetti") {
+        price = price + 5
+    }
+    if (this.size === "large") {
+        price = price + 10
+    } else if (this.size === "medium") {
+        price = price + 5
+    } else {
+        price === price;
+    }
+    return price;
+};
+$(document).ready(function() {
+    var url = $("#pizzaVid").attr('src');
+    $("#pizzaVid").attr('src', '');
+    $("#myModal").on('shown.bs.modal', function() {
+        $("#pizzaVid").attr('src', url);
     });
-  });
+    $("#myModal").on('hide.bs.modal', function() {
+        $("#pizzaVid").attr('src', '');
+    });
+    $("#newPizza").submit(function(event) {
+        event.preventDefault();
+        var size = $("input:radio[name=pizzaSize]:checked").val();
+        var toppings = $("input:radio[name=toppings]:checked").val();
+        var myPizza = new Pizza(size, toppings);
+        $("#showPrice").text(" TOTAL $" + myPizza.price());
+    });
+});
 
-// || "iceCream" || "spaghetti"
+
+
+
+
+
 //
 // ********************************************************************
-//   // $(document).ready(function() {
-//   //   $("#newPizza").submit(function(event) {
-//   //     $("ul").empty();
-//   //     event.preventDefault();
-//   //     var inputtedSize = $("input#pizzaSize").val();
-//   //     var inputtedToppings = $("input#toppings").val();
-//   //     var myPizza = new Pizza(inputtedSize, inputtedToppings);
-//   //     $("#showPrice").text(" TOTAL $" + myPizza.price());
-//   //   });
-//   // });
-// ********************************************************************
+// $(document).ready(function() {
+//   $("#newPizza").submit(function(event) {
+//     $("ul").empty();
+//     event.preventDefault();
+//     var size = $("input:radio[name=pizzaSize]:checked").val();
+//     var toppings = $("input:radio[name=toppings]:checked").val();
+//     var myPizza = new Pizza(size,toppings);
+//     $("#showPrice").text(" TOTAL $" + myPizza.price());
+//   });
+// }); ********************************************************************
