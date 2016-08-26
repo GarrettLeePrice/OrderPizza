993b0c48a92c22ef5b092a621bbf587e4fbc85e6
Garrett Price <garrettleeprice@gmail.com>
2016-08-26 15:00:41 -0700

    exchange text input for radio buttons, and fix back-end logic to add selection prices correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,49 @@
 
-function Pizza(size, toppings) {
+function Pizza(size,toppings) {
   this.size = size;
-  this.toppings = toppings;;
+  this.toppings = toppings;
   };
 
   Pizza.prototype.price = function() {
     var price = 15;
-    if (this.size === "large"){
+      if (this.toppings === "fishSticks") {
+      price = price + 5
+    }  if (this.toppings === "iceCream") {
+      price = price + 5
+    }  if (this.toppings === "spaghetti") {
+      price = price + 5
+    } if (this.size === "large") {
       price = price + 10
     } else if (this.size === "medium") {
       price = price + 5
-    } else if (this.toppings === "Fish Sticks" || "Ice Cream" || "Spaghetti") {
-      price = price + 5
     } else {
-      price = price
+      price === price;
     }
-    return price;
+      return price;
   };
 
   $(document).ready(function() {
     $("#newPizza").submit(function(event) {
       $("ul").empty();
       event.preventDefault();
-      var inputtedSize = $("input#pizzaSize").val();
-      var inputtedToppings = $("input#toppings").val();
-
-      var myPizza = new Pizza(inputtedSize, inputtedToppings);
-      console.log(myPizza);
-      $("#showPrice").append("<li>" + myPizza.price() + "</li>");
+      var size = $("input:radio[name=pizzaSize]:checked").val();
+      var toppings = $("input:radio[name=toppings]:checked").val();
+      var myPizza = new Pizza(size,toppings);
+      $("#showPrice").text(" TOTAL $" + myPizza.price());
     });
   });
+
+// || "iceCream" || "spaghetti"
+//
+// ********************************************************************
+//   // $(document).ready(function() {
+//   //   $("#newPizza").submit(function(event) {
+//   //     $("ul").empty();
+//   //     event.preventDefault();
+//   //     var inputtedSize = $("input#pizzaSize").val();
+//   //     var inputtedToppings = $("input#toppings").val();
+//   //     var myPizza = new Pizza(inputtedSize, inputtedToppings);
+//   //     $("#showPrice").text(" TOTAL $" + myPizza.price());
+//   //   });
+//   // });
+// ********************************************************************
